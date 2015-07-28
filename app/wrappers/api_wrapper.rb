ec046e934a15879a7f761374c30b51bd10bbac0a
farisj <jakefaris@outlook.com>
2015-07-28 14:15:08 -0400

    restructure api calls to wrappers, create restaurant class

---
+++ app/wrappers/api_wrapper.rb
@@ -0,0 +1,20 @@
+class ApiWrapper
+
+  def get_data
+
+    # preliminary 
+    # g = GooglePlacesWrapper.new.search
+    # y = YelpWrapper.new.yelp_search
+    # f = search_f4sqr
+
+    #Combine magic
+      #Take three hashes and .uniq them somehow
+      #Match them up by address
+      #if g.address == y.address && y.address == f.address
+        #it's one place
+      #end
+
+    #present data
+  end
+
+end
--- spec/factories/restaurants.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :restaurant do
-    
-  end
-
-end
