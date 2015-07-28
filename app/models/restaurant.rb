63b8bccb9d86ce02c3ae29a75506fb175ab57594
Becckitt <hello@beccabarton.com>
2015-07-28 11:29:46 -0400

    Adds yelp search to restaurants model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
+  attr_accessor :yelp_results
 
   GOOGLE_API_KEY = ENV['google_places_api_key']
 
@@ -53,5 +54,41 @@ class Restaurant < ActiveRecord::Base
 
       end
     end
+
+    #YELP SEARCHING
+
+    #Get Api keys from Figaro, and define for Yelp gem.
+    def yelp_client
+      @client ||= Yelp::Client.new({
+        consumer_key: ENV["yelp_consumer_key"],
+        consumer_secret: ENV["yelp_consumer_secret"],
+        token: ENV["yelp_token"],
+        token_secret: ENV["yelp_token_secret"]
+        })
+    end
+
+    #Search yelp
+    def yelp_search
+      params = {
+        limit: 10,
+        sort: 2,
+        radius_filter: 1000,
+        category_filter: 'restaurants'
+      }
+      @yelp_results = {}
+      #This line below is in case we decide to go back to searching by zip.
+      #client.search(zipcode, params, { lang: 'en' }).businesses.collect do |business|
+      coordinates = {latitude: 40.704628, longitude: -74.014155}
+      #Search yelp and return hash of restaurant values.
+      yelp_client.search_by_coordinates(coordinates, params, { lang: 'en' }).businesses.collect do |business|
+        @yelp_results= {
+          name: business.name, 
+          yelp_rating: business.rating,
+          image_url: business.snippet_image_url,
+          categories: business.categories,
+          location: "#{business.location.display_address.first}, #{business.location.display_address.last}"
+        }
+      end
+  end
 
 end
