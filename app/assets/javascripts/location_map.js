13a0fe9040bf46f7eb589054a6e8626f59a7bd87
Becckitt <hello@beccabarton.com>
2015-07-29 14:59:30 -0400

    adds the marker and some marker animation

--- app/assets/javascripts/location_map.js
+++ app/assets/javascripts/location_map.js
@@ -11,11 +11,28 @@ $(document).ready(function(){
 });
 
 function insertMap(latitude, longitude) {
+      var myLatLng = new google.maps.LatLng(latitude, longitude);
       var mapCanvas = document.getElementById('map-canvas');
       var mapOptions = {
-        center: new google.maps.LatLng(latitude, longitude),
+        center: myLatLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-      var map = new google.maps.Map(mapCanvas, mapOptions);  
+      var map = new google.maps.Map(mapCanvas, mapOptions); 
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        draggable: true
+      }); 
+      google.maps.event.addListener(marker, 'click', toggleBounce);
+    }
+
+    function toggleBounce() {
+      if (marker.getAnimation() != null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
     }
