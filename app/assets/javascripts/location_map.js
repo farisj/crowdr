fd1201a71c1c53a7a446c31999a7a294fb4d95fc
esmyser <esmyser@gmail.com>
2015-07-29 13:49:04 -0400

    adds map

--- app/assets/javascripts/location_map.js
+++ app/assets/javascripts/location_map.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
         var latitude = position.coords.latitude.toFixed(2);
         var longitude = position.coords.longitude.toFixed(2);
 
-        $('#longitude').text(longitude);
-        $('#latitude').text(latitude);
         insertMap(latitude, longitude);
       });
   }
