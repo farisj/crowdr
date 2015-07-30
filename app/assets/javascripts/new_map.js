9efb5b4d1030525d6f74ca16ddfc041207986d81
esmyser <esmyser@gmail.com>
2015-07-30 14:46:34 -0400

    object orients js

--- app/assets/javascripts/new_map.js
+++ app/assets/javascripts/new_map.js
@@ -9,14 +9,14 @@ function initialize() {
 };
 
 function Map(latitude, longitude) {
-  var myLatLng = new google.maps.LatLng(latitude, longitude);
-      mapCanvas = document.getElementById('map-canvas');
+  var myLatLng = new google.maps.LatLng(latitude, longitude), 
+      mapCanvas = document.getElementById('map-canvas'),
       mapOptions = {
         center: myLatLng,
         zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       },
-      map = new google.maps.Map(mapCanvas, mapOptions), 
+      map = new google.maps.Map(mapCanvas, mapOptions); 
       marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
@@ -24,27 +24,31 @@ function Map(latitude, longitude) {
         draggable: true
       }); 
 
+  google.maps.event.trigger(marker, 'click');
+  google.maps.event.addListener(marker, 'click', toggleBounce);
+};
+
+Map.prototype.markerDrag = function() {
   google.maps.event.addListener(marker, 'dragend', function(){
         var newLatLng = marker.getPosition();
         map.changeCoords(newLatLng);
       });
-  google.maps.event.trigger(marker, 'click');
-  // google.maps.event.addListener(marker, 'click', toggleBounce);
-};
-
-// Map.prototype.toggleBounce = function() {
-//   if (marker.getAnimation() != null) {
-//     marker.setAnimation(null);
-//   } else {
-//     marker.setAnimation(google.maps.Animation.BOUNCE);
-//   }
-// };
-
-Map.prototype.insertHiddens = function(latitude, longitude) {
-  $('#search_lt').val(latitude);
-  $('#search_lg').val(longitude);
 };
 
 Map.prototype.changeCoords = function(newLatLng) {
-  map.insertHiddens(newLatLng.lat(), newLatLng.lng());
+  insertHiddens(newLatLng.lat(), newLatLng.lng());
 };
+
+Map.prototype.insertHiddens = function(latitude, longitude) {
+    $('#search_lt').val(latitude);
+    $('#search_lg').val(longitude);
+};
+
+
+function toggleBounce() {
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
